6a5acae218db638f1780761cbc0ab3f9cc70166a
Fantomasa <ivailoinfo@gmail.com>
2024-08-17 23:51:11 +0300

    create routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from './auth/auth.module';
+import { MovieModule } from './movie/movie.module';
+import { TvModule } from './tv/tv.module';
+import { SearchModule } from './search/search.module';
 import envVars from "./config/envVars";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [envVars] })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [envVars] }), AuthModule, MovieModule, TvModule, SearchModule],
   controllers: [AppController],
   providers: [AppService]
 })
